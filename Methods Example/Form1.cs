0b8e670b8fb6f576648740d5cd66ac696398adf9
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-09-25 22:46:45 +0300

    Add files via upload

---
+++ Methods Example/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metodlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HesMak hm = new HesMak();
+            hm.mesaj();
+            this.Text = Convert.ToString(hm.toplam());
+            this.Text = "Methods";
+            label1.Text = hm.toplam().ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            HesMak.duragan();
+        }
+    }
+}
---
+++ Methods Example/HesMak.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metodlar
+{
+    class HesMak
+    {
+        public HesMak()
+        {
+            MessageBox.Show("Yapıcı Metod çalıştı.");
+        }
+
+        public void mesaj()
+        {
+            MessageBox.Show("void metod");
+        }
+
+        public static void duragan()
+        {
+            MessageBox.Show("bu bir duragan bir metottur.");
+        }
+
+        int[] sayilar = { 1, 4, 6, 8, 9 };
+
+        public int toplam()
+        {
+            Random r = new Random();
+            int toplam = sayilar[r.Next(0, 6)] * 5;
+            return toplam;
+        }
+        
+        ~HesMak ()
+        {
+            MessageBox.Show("Yıkıcı metot çalıştı.");
+
+        }
+    }
+}
