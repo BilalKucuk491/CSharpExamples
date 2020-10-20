ef0af035b1e561cc8043ef21a983ba5b12ca6004
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-10-20 14:38:02 +0300

    Add files via upload

---
+++ gorsel2.hafta/gorsel2.hafta/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace gorsel2.hafta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTcNo = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnKayitEkle = new System.Windows.Forms.Button();
+            this.dGridAdresler = new System.Windows.Forms.DataGridView();
+            this.btnKayitSil = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dGridAdresler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTcNo
+            // 
+            this.txtTcNo.Location = new System.Drawing.Point(132, 46);
+            this.txtTcNo.Multiline = true;
+            this.txtTcNo.Name = "txtTcNo";
+            this.txtTcNo.Size = new System.Drawing.Size(170, 36);
+            this.txtTcNo.TabIndex = 0;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(133, 89);
+            this.txtAd.Multiline = true;
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(261, 36);
+            this.txtAd.TabIndex = 1;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(133, 173);
+            this.txtAdres.Multiline = true;
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(581, 36);
+            this.txtAdres.TabIndex = 3;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(133, 131);
+            this.txtSoyad.Multiline = true;
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(260, 36);
+            this.txtSoyad.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tc No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Soyad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Adres";
+            // 
+            // btnKayitEkle
+            // 
+            this.btnKayitEkle.Location = new System.Drawing.Point(133, 227);
+            this.btnKayitEkle.Name = "btnKayitEkle";
+            this.btnKayitEkle.Size = new System.Drawing.Size(92, 33);
+            this.btnKayitEkle.TabIndex = 8;
+            this.btnKayitEkle.Text = "Kayıt Ekle";
+            this.btnKayitEkle.UseVisualStyleBackColor = true;
+            this.btnKayitEkle.Click += new System.EventHandler(this.btnKayitEkle_Click);
+            // 
+            // dGridAdresler
+            // 
+            this.dGridAdresler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGridAdresler.Location = new System.Drawing.Point(12, 274);
+            this.dGridAdresler.Name = "dGridAdresler";
+            this.dGridAdresler.RowHeadersWidth = 51;
+            this.dGridAdresler.RowTemplate.Height = 24;
+            this.dGridAdresler.Size = new System.Drawing.Size(821, 267);
+            this.dGridAdresler.TabIndex = 9;
+            // 
+            // btnKayitSil
+            // 
+            this.btnKayitSil.Location = new System.Drawing.Point(249, 227);
+            this.btnKayitSil.Name = "btnKayitSil";
+            this.btnKayitSil.Size = new System.Drawing.Size(94, 33);
+            this.btnKayitSil.TabIndex = 10;
+            this.btnKayitSil.Text = "Kayıt Sil";
+            this.btnKayitSil.UseVisualStyleBackColor = true;
+            this.btnKayitSil.Click += new System.EventHandler(this.btnKayitSil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 553);
+            this.Controls.Add(this.btnKayitSil);
+            this.Controls.Add(this.dGridAdresler);
+            this.Controls.Add(this.btnKayitEkle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtTcNo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Adres Kayıt Formu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dGridAdresler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTcNo;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnKayitEkle;
+        private System.Windows.Forms.DataGridView dGridAdresler;
+        private System.Windows.Forms.Button btnKayitSil;
+    }
+}
+
---
+++ gorsel2.hafta/gorsel2.hafta/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace gorsel2.hafta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void baglan()
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+        OleDbConnection con = new OleDbConnection();
+        OleDbCommand komut = new OleDbCommand();
+        DataSet ds = new DataSet();
+        public void verilerigetir()
+        {
+            try
+            {
+
+                baglan();
+                komut.CommandText = "select*from Adresler";
+                OleDbDataAdapter verial = new OleDbDataAdapter();
+                verial.SelectCommand = komut;
+                ds.Clear();
+                verial.Fill(ds, "Adresler");
+                dGridAdresler.DataSource = ds.Tables["Adresler"];
+                con.Close();
+            }
+
+            catch (Exception hata)
+            {
+                MessageBox.Show("Hata", hata.ToString());
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=AdresDefteri.mdb";
+            komut.Connection = con;
+            verilerigetir();
+
+        }
+
+        private void btnKayitEkle_Click(object sender, EventArgs e)
+        {
+            komut.CommandText = "insert into Adresler(tcNo,Ad,Soyad,Adres) values('" + txtTcNo.Text + "','" + txtAd.Text + "','" + txtSoyad.Text + "','" + txtAdres.Text + "')";
+            baglan();
+            if (komut.ExecuteNonQuery() == 1)
+                MessageBox.Show("Kayıt Eklendi");
+            else
+                MessageBox.Show("Kayıt Eklenmedi");
+            con.Close();
+            verilerigetir();
+        }
+
+        private void btnKayitSil_Click(object sender, EventArgs e)
+        {
+            komut.CommandText = "Delete from Adresler where TcNo='" + txtTcNo.Text + "'";
+            baglan();
+            if (komut.ExecuteNonQuery() == 1)
+                MessageBox.Show("Kayıt Silindi");
+            else
+                MessageBox.Show("Kayıt Silinmedi");
+            con.Close();
+            verilerigetir();
+
+        }
+    }
+}
